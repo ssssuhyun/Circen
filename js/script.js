9f11a32359a2b214f05593041aaac272c365b63d
ssssuhyun <133993523+ssssuhyun@users.noreply.github.com>
2023-05-25 11:45:18 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
 $('.submenu').hide();
-// $('#gnb li').mouseenter(function () {
-//     $(this).children('.submenu').stop().slideDown()
-// });
-// $('#gnb li').mouseleave(function () {
-//     $(this).children('.submenu').stop().fadeOut()
-// });
 $(function(){
     $('#gnb li').hover(function(){
       $(this).find('.submenu').slideToggle();
